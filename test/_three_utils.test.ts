9c89a536d9aa8550e34fb1cdb732f9e43a84ea7c
Yusuke Kawamoto <novogrammer@gmail.com>
2024-06-17 14:36:12 +0900

    _three_utils.tsも追加

---
+++ test/_three_utils.test.ts
@@ -0,0 +1,13 @@
+import {
+  calcCameraZ,
+} from '../src/assets/js/_three_utils';
+
+
+describe('calcCameraZ', () => {
+  test('calcCameraZ(100,90) equal 50', () => {
+    expect(calcCameraZ(100, 90)).toBeCloseTo(50, 5);
+  });
+  test('calcCameraZ(100,60) equal (50 / Math.sqrt(3)) ', () => {
+    expect(calcCameraZ(100, 60)).toBeCloseTo(50 * Math.sqrt(3), 5);
+  });
+});
