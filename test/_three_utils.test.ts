b88f9401ef1100eb6fb09371c6fbd66d5dbe7a49
Yusuke Kawamoto <novogrammer@gmail.com>
2025-06-05 15:42:53 +0900

    docs: correct calcCameraZ comment

--- test/_three_utils.test.ts
+++ test/_three_utils.test.ts
@@ -7,7 +7,7 @@ describe('calcCameraZ', () => {
   test('calcCameraZ(100,90) equal 50', () => {
     expect(calcCameraZ(100, 90)).toBeCloseTo(50, 5);
   });
-  test('calcCameraZ(100,60) equal (50 / Math.sqrt(3)) ', () => {
+  test('calcCameraZ(100,60) equal (50 * Math.sqrt(3)) ', () => {
     expect(calcCameraZ(100, 60)).toBeCloseTo(50 * Math.sqrt(3), 5);
   });
 });
